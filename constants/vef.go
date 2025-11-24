b97326f398f0724468efac43dbf75ddb79408610
Venus <iweixiaopeng@gmail.com>
2025-11-24 17:31:09 +0800

    chore: bump version to v0.15.1

--- constants/vef.go
+++ constants/vef.go
@@ -3,5 +3,5 @@ package constants
 const (
 	// VEFVersion represents the current version of the VEF Framework.
 	// This version number follows semantic versioning (semver) format.
-	VEFVersion = "v0.15.0"
+	VEFVersion = "v0.15.1"
 )
--- internal/middleware/content_type.go
+++ internal/middleware/content_type.go
@@ -15,6 +15,7 @@ func NewContentTypeMiddleware() app.Middleware {
 			if method != fiber.MethodPost && method != fiber.MethodPut ||
 				webhelpers.IsJson(ctx) ||
 				webhelpers.IsMultipart(ctx) {
+
 				return ctx.Next()
 			}
 
