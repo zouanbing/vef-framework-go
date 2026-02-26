67b5d886981c4ded928b6ebf1f7473c0f8cbf6be
Venus <iweixiaopeng@163.com>
2026-02-26 21:59:05 +0800

    refactor(approval): update module to use dispatcher and add auto-migration hook

--- internal/approval/module.go
+++ internal/approval/module.go
@@ -1,14 +1,19 @@
 package approval
 
 import (
+	"context"
+
 	"go.uber.org/fx"
 
+	"github.com/ilxqx/vef-framework-go/config"
 	"github.com/ilxqx/vef-framework-go/internal/approval/engine"
-	"github.com/ilxqx/vef-framework-go/internal/approval/publisher"
+	"github.com/ilxqx/vef-framework-go/internal/approval/migration"
+	"github.com/ilxqx/vef-framework-go/internal/approval/dispatcher"
 	"github.com/ilxqx/vef-framework-go/internal/approval/resource"
 	"github.com/ilxqx/vef-framework-go/internal/approval/service"
 	"github.com/ilxqx/vef-framework-go/internal/approval/strategy"
 	"github.com/ilxqx/vef-framework-go/internal/approval/timeout"
+	"github.com/ilxqx/vef-framework-go/orm"
 )
 
 // Module is the approval workflow engine module.
@@ -17,8 +22,18 @@ var Module = fx.Module(
 
 	strategy.Module,
 	engine.Module,
-	publisher.Module,
+	dispatcher.Module,
 	service.Module,
 	resource.Module,
 	timeout.Module,
+
+	fx.Invoke(autoMigrate),
 )
+
+func autoMigrate(ctx context.Context, cfg *config.ApprovalConfig, db orm.DB, ds *config.DataSourceConfig) error {
+	if !cfg.AutoMigrate {
+		return nil
+	}
+
+	return migration.Migrate(ctx, db, ds.Kind)
+}
