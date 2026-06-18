d39a5673709220fb5fa726de23b4f61adbb23f18
Venus <coldsmirk@163.com>
2026-06-18 01:29:55 -0700

    refactor(config)!: rename EnvKeyPrefix to EnvPrefix

--- config/env.go
+++ config/env.go
@@ -2,9 +2,9 @@ package config
 
 // Environment variable keys.
 const (
-	EnvKeyPrefix    = "VEF"
-	EnvNodeID       = EnvKeyPrefix + "_NODE_ID"       // Snowflake generator node identifier
-	EnvLogLevel     = EnvKeyPrefix + "_LOG_LEVEL"     // Log level (debug|info|warn|error)
-	EnvConfigPath   = EnvKeyPrefix + "_CONFIG_PATH"   // Custom config file path
-	EnvI18NLanguage = EnvKeyPrefix + "_I18N_LANGUAGE" // Override default language
+	EnvPrefix       = "VEF"
+	EnvNodeID       = EnvPrefix + "_NODE_ID"       // Snowflake generator node identifier
+	EnvLogLevel     = EnvPrefix + "_LOG_LEVEL"     // Log level (debug|info|warn|error)
+	EnvConfigPath   = EnvPrefix + "_CONFIG_PATH"   // Custom config file path
+	EnvI18NLanguage = EnvPrefix + "_I18N_LANGUAGE" // Override default language
 )
